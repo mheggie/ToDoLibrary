69ef25eae5a0649503b164c336f1de7bc922214c
Mike <mhegs42@yahoo.com.au>
2020-07-04 10:27:39 +1000

    Add project files.

---
+++ ToDoDataAccess/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoDataAccess.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string DueDate { get; set; }
+        public string CompletedDate { get; set; }
+
+    }
+}
---
+++ ToDoDataAccess/dbo/ToDoDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoDataAccess.dbo
+{
+    public class ToDoDataAccess
+    {
+
+    }
+}
